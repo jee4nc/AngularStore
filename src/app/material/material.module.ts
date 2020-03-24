1c8ea0edf91f0774259f636d56e9e4ec4ced94bf
jean <jeanc.aldea1@gmail.com>
2020-03-24 16:05:02 -0300

    cards material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatCardModule} from '@angular/material/card';
 
 
 
@@ -14,13 +15,15 @@ import {MatBadgeModule} from '@angular/material/badge';
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
