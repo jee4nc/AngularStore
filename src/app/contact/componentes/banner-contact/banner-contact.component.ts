faf778f86f1bf2325e5afb1f12e302193ffbcfc7
jean <jeanc.aldea1@gmail.com>
2020-03-24 20:31:19 -0300

    contact finished

---
+++ src/app/contact/componentes/banner-contact/banner-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-contact',
+  templateUrl: './banner-contact.component.html',
+  styleUrls: ['./banner-contact.component.scss']
+})
+export class BannerContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -4,11 +4,14 @@ import { ContactComponent } from './componentes/contact/contact.component';
 import { SharedModule } from '../shared/shared.module';
 import { ContactRoutingModule } from './contact-routing.module';
 import { SaludoComponent } from './componentes/saludo/saludo.component';
+import {  BannerContactComponent} from './componentes/banner-contact/banner-contact.component';
+
 
 @NgModule({
     declarations: [
         ContactComponent,
         SaludoComponent,
+        BannerContactComponent
     ],
     imports: [
         CommonModule,
